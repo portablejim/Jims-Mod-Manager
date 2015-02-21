70be8068e0e74caddf28fd1785824985863a2eb5
James McKay <dcseee-github@yahoo.com.au>
2015-02-21 14:56:24 +1100

    Make the normal launch work.

--- src/main/java/portablejim/jimsmodmanager/ModManager.java
+++ src/main/java/portablejim/jimsmodmanager/ModManager.java
@@ -16,6 +16,8 @@ public class ModManager implements ITweaker {
         LogWrapper.info("ModManager: acceptOptions()");
         Model model = new Model();
         model.setSide(Model.SIDE.CLIENT);
+        ManagerBackend backend = new ManagerBackend(model, gameDir);
+        backend.getLocalModpack("jmm-modpack", "modpack.json");
     }
 
     @Override
